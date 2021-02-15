38c97ea6032bc21c23089e1c90d6c93d157cf498
dejan.subasic <dejan.subasic@gmail.com>
2021-02-15 23:11:16 +0100

    Changed PowerSystemResource to match model

--- NetworkModelService/DataModel/Core/PowerSystemResource.cs
+++ NetworkModelService/DataModel/Core/PowerSystemResource.cs
@@ -12,14 +12,16 @@ namespace FTN.Services.NetworkModelService.DataModel.Core
 {
 	public class PowerSystemResource : IdentifiedObject
 	{
+		/*
 		private long location = 0;
 		private string customType = string.Empty;		
-		
+		*/
+
 		public PowerSystemResource(long globalId)
 			: base(globalId)
 		{
-		}	
-
+		}
+		/*
 		public string CustomType
 		{
 			get 
@@ -45,13 +47,13 @@ namespace FTN.Services.NetworkModelService.DataModel.Core
 				location = value;
 			}
 		}
-
+		*/
 		public override bool Equals(object obj)
 		{
 			if (base.Equals(obj))
 			{
 				PowerSystemResource x = (PowerSystemResource)obj;
-				return (x.customType == this.customType && x.location == this.location);
+				return true;
 			}
 			else
 			{
@@ -64,16 +66,17 @@ namespace FTN.Services.NetworkModelService.DataModel.Core
 			return base.GetHashCode();
 		}		
 
-		#region IAccess implementation
+
 
 		public override bool HasProperty(ModelCode property)
 		{
 			switch (property)
 			{
+				/*
 				case ModelCode.PSR_CUSTOMTYPE:
 				case ModelCode.PSR_LOCATION:
 					return true;
-
+				*/
 				default:
 					return base.HasProperty(property);
 			}
@@ -83,6 +86,7 @@ namespace FTN.Services.NetworkModelService.DataModel.Core
 		{
 			switch (property.Id)
 			{
+				/*
 				case ModelCode.PSR_CUSTOMTYPE:					
 					property.SetValue(customType);
 					break;
@@ -90,7 +94,7 @@ namespace FTN.Services.NetworkModelService.DataModel.Core
 				case ModelCode.PSR_LOCATION:
 					property.SetValue(location);
 					break;
-
+				*/
 				default:
 					base.GetProperty(property);
 					break;
@@ -101,6 +105,7 @@ namespace FTN.Services.NetworkModelService.DataModel.Core
 		{
 			switch (property.Id)
 			{
+				/*
 				case ModelCode.PSR_CUSTOMTYPE:
 					customType = property.AsString();
 					break;
@@ -108,28 +113,25 @@ namespace FTN.Services.NetworkModelService.DataModel.Core
 				case ModelCode.PSR_LOCATION:
 					location = property.AsReference();
 					break;
-
+				*/
 				default:
 					base.SetProperty(property);
 					break;
 			}
 		}
 
-		#endregion IAccess implementation
-
-		#region IReference implementation
 		
 		public override void GetReferences(Dictionary<ModelCode, List<long>> references, TypeOfReference refType)
 		{
+			/*
 			if (location != 0 && (refType == TypeOfReference.Reference || refType == TypeOfReference.Both))
 			{
 				references[ModelCode.PSR_LOCATION] = new List<long>();
 				references[ModelCode.PSR_LOCATION].Add(location);
 			}
-			
+			*/
+
 			base.GetReferences(references, refType);			
 		}
-
-		#endregion IReference implementation		
 	}
 }
