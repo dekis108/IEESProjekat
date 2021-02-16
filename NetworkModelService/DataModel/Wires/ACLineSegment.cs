75aa1d61f4c80897a562434004ebacfe79a673f2
dejan.subasic <dejan.subasic@gmail.com>
2021-02-16 07:48:45 +0100

    Added ACLineSegment class

---
+++ NetworkModelService/DataModel/Wires/ACLineSegment.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FTN.Common;
+using FTN.Services.NetworkModelService.DataModel.Core;
+
+namespace FTN.Services.NetworkModelService.DataModel.Wires
+{
+    public class ACLineSegment : Conductor
+    {
+        public long B0CH { get; set; }
+        public long BCH { get; set; }
+        public long G0CH { get; set; }
+        public long GCH { get; set; }
+
+        public long R { get; set; }
+        public long R0 { get; set; }
+        public long X { get; set; }
+
+        public long X0 { get; set; }
+
+        public List<long> ACLineSegmentPhases { get; set; }
+
+
+        public ACLineSegment(long globalId) : base(globalId)
+		{
+			ACLineSegmentPhases = new List<long>();
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (base.Equals(obj))
+			{
+				ACLineSegment x = (ACLineSegment)obj;
+				return CompareHelper.CompareLists(this.ACLineSegmentPhases, x.ACLineSegmentPhases) && x.B0CH == this.B0CH &&
+					x.BCH == this.BCH && x.G0CH == this.G0CH && x.GCH == this.GCH && x.R == this.R && x.R0 == this.R0 && x.X == this.X && x.X0 == this.X0;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		public override bool HasProperty(ModelCode property)
+		{
+			switch (property)
+			{
+				case ModelCode.ACLINESEGMENT_B0CH:
+				case ModelCode.ACLINESEGMENT_BCH:
+				case ModelCode.ACLINESEGMENT_G0CH:
+				case ModelCode.ACLINESEGMENT_GCH:
+				case ModelCode.ACLINESEGMENT_PHASES:
+				case ModelCode.ACLINESEGMENT_R:
+				case ModelCode.ACLINESEGMENT_R0:
+				case ModelCode.ACLINESEGMENT_X:
+				case ModelCode.ACLINESEGMENT_X0:
+					return true;
+				default:
+					return base.HasProperty(property);
+			}
+		}
+
+		public override void GetProperty(Property prop)
+		{
+			switch (prop.Id)
+			{
+				case ModelCode.ACLINESEGMENT_B0CH:
+					prop.SetValue(B0CH);
+					break;
+				case ModelCode.ACLINESEGMENT_BCH:
+					prop.SetValue(BCH);
+					break;
+				case ModelCode.ACLINESEGMENT_G0CH:
+					prop.SetValue(G0CH);
+					break;
+				case ModelCode.ACLINESEGMENT_GCH:
+					prop.SetValue(GCH);
+					break;
+				case ModelCode.ACLINESEGMENT_PHASES:
+					prop.SetValue(ACLineSegmentPhases);
+					break;
+				case ModelCode.ACLINESEGMENT_R:
+					prop.SetValue(R);
+					break;
+				case ModelCode.ACLINESEGMENT_R0:
+					prop.SetValue(R0);
+					break;
+				case ModelCode.ACLINESEGMENT_X:
+					prop.SetValue(X);
+					break;
+				case ModelCode.ACLINESEGMENT_X0:
+					prop.SetValue(X0);
+					break;
+				default:
+					base.GetProperty(prop);
+					break;
+			}
+		}
+
+		public override void SetProperty(Property property)
+		{
+			switch (property.Id)
+			{
+				case ModelCode.ACLINESEGMENT_B0CH:
+					B0CH = property.AsLong();
+					break;
+				case ModelCode.ACLINESEGMENT_BCH:
+					BCH = property.AsLong();
+					break;
+				case ModelCode.ACLINESEGMENT_G0CH:
+					G0CH = property.AsLong();
+					break;
+				case ModelCode.ACLINESEGMENT_GCH:
+					GCH = property.AsLong();
+					break;
+				case ModelCode.ACLINESEGMENT_R:
+					R = property.AsLong();
+					break;
+				case ModelCode.ACLINESEGMENT_R0:
+					R0 = property.AsLong();
+					break;
+				case ModelCode.ACLINESEGMENT_X:
+					X = property.AsLong();
+					break;
+				case ModelCode.ACLINESEGMENT_X0:
+					X0 = property.AsLong();
+					break;
+				default:
+					base.SetProperty(property);
+					break;
+			}
+		}
+
+		public override bool IsReferenced
+		{
+			get
+			{
+				return ACLineSegmentPhases.Count > 0 || base.IsReferenced;
+			}
+		}
+
+		public override void GetReferences(Dictionary<ModelCode, List<long>> references, TypeOfReference refType)
+		{
+			if (ACLineSegmentPhases != null && ACLineSegmentPhases.Count > 0 && (refType == TypeOfReference.Target || refType == TypeOfReference.Both))
+			{
+				references[ModelCode.ACLINESEGMENT_PHASES] = ACLineSegmentPhases.GetRange(0, ACLineSegmentPhases.Count);
+			}
+
+			base.GetReferences(references, refType);
+		}
+
+		public override void AddReference(ModelCode referenceId, long globalId)
+		{
+			switch (referenceId)
+			{
+				case ModelCode.ACLINESEGMENTPHASE_ACLINESEGMENT:
+					ACLineSegmentPhases.Add(globalId);
+					break;
+
+				default:
+					base.AddReference(referenceId, globalId);
+					break;
+			}
+		}
+
+		public override void RemoveReference(ModelCode referenceId, long globalId)
+		{
+			switch (referenceId)
+			{
+				case ModelCode.ACLINESEGMENTPHASE_ACLINESEGMENT:
+
+					if (ACLineSegmentPhases.Contains(globalId))
+					{
+						ACLineSegmentPhases.Remove(globalId);
+					}
+					else
+					{
+						CommonTrace.WriteTrace(CommonTrace.TraceWarning, "Entity (GID = 0x{0:x16}) doesn't contain reference 0x{1:x16}.", this.GlobalId, globalId);
+					}
+
+					break;
+
+				default:
+					base.RemoveReference(referenceId, globalId);
+					break;
+			}
+		}
+
+	}
+}
